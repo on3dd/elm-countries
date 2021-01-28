27db9bdcfa50b6c34a0b238b49dc0b3a7be66f40
on3dd <onedeadwave@gmail.com>
2021-01-28 23:03:28 +1000

    styles update 1/2

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,11 @@
 module Main exposing (Model, Msg(..), init, main, search, searchDecoder, subscriptions, update, view, viewResponse)
 
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Css exposing (..)
+import Html
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (class, classList, css, placeholder, src, value)
+import Html.Styled.Events exposing (onClick, onInput)
 import Http
 import Json.Decode as D exposing (Decoder, field, list, map2, string)
 
@@ -28,7 +30,7 @@ main =
         { init = init
         , update = update
         , subscriptions = subscriptions
-        , view = view
+        , view = view >> toUnstyled
         }
 
 
@@ -96,9 +98,47 @@ subscriptions _ =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h2 [] [ text "Countries" ]
-        , viewResponse model
+    div
+        [ class "app"
+        , css
+            [ width (vw 100)
+            , fontSize (px 16)
+            , fontFamilies
+                [ "Segoe UI"
+                , "Frutiger"
+                , "Frutiger Linotype"
+                , "Dejavu Sans"
+                , "Helvetica Neue"
+                , "Arial"
+                , "sans-serif"
+                ]
+            , overflowX hidden
+            ]
+        ]
+        [ div
+            [ class "app__container"
+            , css
+                [ width (pct 100)
+                , maxWidth (px 500)
+                , margin2 (px 0) auto
+                , textAlign center
+                ]
+            ]
+            [ h2
+                [ class "app__heading"
+                , css
+                    [ margin2 (rem 1) (px 0) ]
+                ]
+                [ text "Countries" ]
+            , div
+                [ class "app__view"
+                , css
+                    [ width inherit
+                    , maxWidth (px 500)
+                    ]
+                ]
+                [ viewResponse model ]
+            ]
         ]
 
 
@@ -106,31 +146,69 @@ viewResponse : Model -> Html Msg
 viewResponse model =
     case model.status of
         Failure ->
-            div []
-                [ text "I could not load a list of countries for some reason. "
-                , button [ onClick Reload ] [ text "Try Again!" ]
+            div
+                [ classList
+                    [ ( "app__search", True )
+                    , ( "app__search-failure", True )
+                    ]
+                ]
+                [ span
+                    [ class "search__error" ]
+                    [ text "I could not load a list of countries for some reason. " ]
+                , button
+                    [ class "search__text", onClick Reload ]
+                    [ text "Try Again!" ]
                 ]
 
         Loading ->
-            div []
+            div
+                [ classList
+                    [ ( "app__search", True )
+                    , ( "app__search-loading", True )
+                    ]
+                , css
+                    [ displayFlex
+                    , flexDirection column
+                    , alignItems center
+                    ]
+                ]
                 [ input
                     [ placeholder "Enter the country name"
-                    , style "display" "block"
                     , value model.content
                     , onInput Search
-                    , disabled True
+                    , class "search__input"
+                    , css
+                        [ display block
+                        , marginBottom (rem 0.5)
+                        ]
                     ]
                     []
-                , text "Loading..."
+                , span
+                    [ class "search__input" ]
+                    [ text "Loading..." ]
                 ]
 
         Success list ->
-            div []
+            div
+                [ classList
+                    [ ( "app__search", True )
+                    , ( "app__search-success", True )
+                    ]
+                , css
+                    [ displayFlex
+                    , flexDirection column
+                    , alignItems center
+                    ]
+                ]
                 [ input
                     [ placeholder "Enter the country name"
-                    , style "display" "block"
                     , value model.content
                     , onInput Search
+                    , class "search__input"
+                    , css
+                        [ display block
+                        , marginBottom (rem 2)
+                        ]
                     ]
                     []
                 , viewList list
@@ -140,15 +218,43 @@ viewResponse model =
 viewList : List Country -> Html Msg
 viewList countries =
     ul
-        []
+        [ class "app__list"
+        , css
+            [ margin (px 0)
+            , padding (px 0)
+            , width (pct 100)
+            ]
+        ]
         (List.map viewItem countries)
 
 
 viewItem : Country -> Html Msg
 viewItem country =
-    div []
-        [ text country.name
-        , img [ src country.flag ] []
+    li
+        [ class "app__item"
+        , css
+            [ display block
+            , marginBottom (rem 1)
+            , overflowX hidden
+            ]
+        ]
+        [ span
+            [ class "item__text"
+            , css
+                [ display block
+                , marginBottom (rem 0.5)
+                ]
+            ]
+            [ text country.name ]
+        , img
+            [ src country.flag
+            , class "item__flag"
+            , css
+                [ display block
+                , width (pct 100)
+                ]
+            ]
+            []
         ]
 
 
