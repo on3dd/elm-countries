19e578c42d22f73779395b87b3a2b5ab92ea1383
on3dd <onedeadwave@gmail.com>
2021-01-28 17:06:21 +1000

    http test

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,85 @@
-module Main exposing (..)
+module Main exposing (Model(..), Msg(..), init, main, subscriptions, update, view)
 
-import Browser exposing (sandbox)
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Browser
+import Html exposing (Html, pre, text)
+import Http
 
-main : Program () Int Msg
+
+
+-- MAIN
+
+
+main : Program () Model Msg
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
-type Msg = Increment | Decrement
 
-update : Msg -> number -> number
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
 
-    Decrement ->
-      model - 1
+-- MODEL
 
+
+type Model
+    = Failure
+    | Loading
+    | Success String
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading
+    , Http.get
+        { url = "https://elm-lang.org/assets/public-opinion.txt"
+        , expect = Http.expectString GotText
+        }
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = GotText (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg _ =
+    case msg of
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( Success fullText, Cmd.none )
+
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    case model of
+        Failure ->
+            text "I was unable to load your book."
+
+        Loading ->
+            text "Loading..."
+
+        Success fullText ->
+            pre [] [ text fullText ]
